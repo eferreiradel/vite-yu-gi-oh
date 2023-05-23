244dcea2a44cc7cf937c2fb702c1f2ae8f738640
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-05-24 00:32:10 +0200

    first commit

---
+++ src/components/app-card_render.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-fluid">
+    <div class="row p-0">
+      <div class="col-12 col-lg-6 p-0">
+        <div class="col-4 col-lg-5 p-3">
+          <div class="card"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "card_render",
+  props: ["cardList"],
+  data() {
+    return {};
+  },
+  mounted() {
+    console.log(cardLIst);
+  },
+};
+</script>
+<style></style>
