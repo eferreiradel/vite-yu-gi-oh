daf48ab49501ea4db4d44d0906b9074786a8b265
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-05-24 08:58:41 +0200

    reasponsive App

--- src/components/app-card_render.vue
+++ src/components/app-card_render.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="container-fluid">
-    <div class="row p-0">
-      <div class="col-12 col-lg-6 p-0">
-        <div class="col-4 col-lg-5 p-3">
-          <div class="card"></div>
+    <div class="row justify-content-center">
+      <div class="col-12 bg-light">
+        <div class="row justify-content-center p-0 bg-dark">
+          <div class="col-4 col-lg-3 p-3" v-for="(card, i) in cardList">
+            <div class="card">
+              <div class="container p-0">
+                <img
+                  :src="cardList[i].card_images[0].image_url"
+                  class="img-fluid"
+                />
+              </div>
+            </div>
+            <div class="card-footer py-2">
+              <div class="container text-center">
+                <h5>
+                  {{ cardList[i].name }}
+                </h5>
+              </div>
+              <div class="container p-2 text-center">
+                <span>
+                  {{ cardList[i].archetype }}
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -14,10 +35,9 @@ export default {
   name: "card_render",
   props: ["cardList"],
   data() {
-    return {};
-  },
-  mounted() {
-    console.log(cardLIst);
+    return {
+      lista: [1, 2, 3],
+    };
   },
 };
 </script>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+
+//style
+import "./assets/style/base.scss";
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");
